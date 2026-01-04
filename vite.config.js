63b6599ba373ec856d8d7a2ebf50a612bf86c87e
Thenintheyes <102641421+Thenintheyes@users.noreply.github.com>
2026-01-04 10:36:44 +0530

    Add deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // IMPORTANT: Replace 'hydro-flow' with your EXACT repo name if it's different
+  base: "/hydro-flow/", 
 })
